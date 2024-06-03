ea413d28d2382a5b7ba6be6a53f9e0b82c779c5c
onwukadavid <onwukadavid21@gmail.com>
2024-06-03 18:21:12 +0100

    update: split desktop and mobile nav into multiple components

---
+++ resources/views/components/mobile/profile-dropdown-list.blade.php
@@ -0,0 +1,1 @@
+<a {{$attributes(["class"=>"block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"])}} >{{ $slot }}</a>
---
+++ resources/views/components/profile/profile-dropdown.blade.php
@@ -0,0 +1,32 @@
+<div class="relative ml-3">
+    <div>
+        <button type="button" class="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
+            <span class="absolute -inset-1.5"></span>
+            <span class="sr-only">Open user menu</span>
+            <img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+        </button>
+    </div>
+
+    <div id="dropdown-menu" class="hidden absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
+        <!-- Active: "bg-gray-100", Not Active: "" -->
+        <x-profile.profile-dropdown-list href="#" id="user-menu-item-0">Your Profile</x-profile->
+        <x-profile.profile-dropdown-list href="#" id="user-menu-item-1">Settings</x-profile->
+        <x-profile.profile-dropdown-list href="#" id="user-menu-item-2">Sign out</x-profile->
+    </div>
+</div>
+
+<script>
+    const userMenuButton = document.getElementById('user-menu-button');
+    const dropdownMenu = document.getElementById('dropdown-menu');
+
+    userMenuButton.addEventListener('click', () => {
+        dropdownMenu.classList.toggle('hidden');
+    });
+
+    // Close the dropdown menu when clicking outside
+    window.addEventListener('click', (e) => {
+        if (!userMenuButton.contains(e.target) && !dropdownMenu.contains(e.target)) {
+            dropdownMenu.classList.add('hidden');
+        }
+    });
+</script>
